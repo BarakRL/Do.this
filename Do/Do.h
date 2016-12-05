53a7e3952d1cb2254fb0a5ca288fcd1824c28ef2
Barak Harel <dev@barakharel.com>
2016-12-05 12:15:19 -0800

    Initial Commit

---
+++ Do/Do.h
@@ -0,0 +1,19 @@
+//
+//  Do.h
+//  Do
+//
+//  Created by Barak Harel on 05/12/2016.
+//  Copyright © 2016 Barak Harel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Do.
+FOUNDATION_EXPORT double DoVersionNumber;
+
+//! Project version string for Do.
+FOUNDATION_EXPORT const unsigned char DoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Do/PublicHeader.h>
+
+
