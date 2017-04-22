a5e464a9b00a9fa682178ebfec74a3cbfe2c2027
Thomas Bonk <thomas@meandmymac.de>
2017-04-22 18:08:21 +0200

    Added macOS target

---
+++ DoThis-macOS/DoThis-macOS.h
@@ -0,0 +1,19 @@
+//
+//  DoThis-macOS.h
+//  DoThis-macOS
+//
+//  Created by Thomas Bonk on 22.04.17.
+//  Copyright © 2017 Barak Harel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DoThis-macOS.
+FOUNDATION_EXPORT double DoThis_macOSVersionNumber;
+
+//! Project version string for DoThis-macOS.
+FOUNDATION_EXPORT const unsigned char DoThis_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoThis_macOS/PublicHeader.h>
+
+
